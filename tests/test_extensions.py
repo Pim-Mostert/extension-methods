e8950a7cc64bae891913a84c50f452e29a1c8a6e
Pim Mostert <pim.mostert@pimmostert.com>
2026-02-05 21:35:47 +0100

    Add more tests

--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -1,16 +1,53 @@
+import pytest
+
 from extension_methods.extensions import extension
 
 
 def test_extension():
     # Assign
-    input = 7
-
-    # Act
     @extension(to=int)
     def double(x: int) -> int:
         return x * 2
 
-    output = input | double()
+    # Act
+    output = 7 | double()
 
     # Assert
-    assert 2 * input == output
+    assert output == 7 * 2
+
+
+def test_extension_single_parameter():
+    # Assign
+    @extension(to=int)
+    def add(x: int, to_add: int) -> int:
+        return x + to_add
+
+    # Act
+    output = 7 | add(11)
+
+    # Assert
+    assert 7 + 11 == output
+
+
+def test_extension_two_parameters():
+    # Assign
+    @extension(to=int)
+    def add_then_multiply(x: int, to_add: int, to_multiply: int) -> int:
+        return (x + to_add) * to_multiply
+
+    # Act
+    output = 7 | add_then_multiply(11, 3)
+
+    # Assert
+    assert (7 + 11) * 3 == output
+
+
+def test_extension_wrong_type():
+    # Assign
+    @extension(to=str)
+    def upper(x: str) -> str:
+        return x.upper()
+
+    # Act
+    with pytest.raises(TypeError):
+        _ = 1 | upper()  # pyright: ignore[reportOperatorIssue]
